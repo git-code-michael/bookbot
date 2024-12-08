493e4d65cbc4ae73477eab28c29aff7de0e2f0ca
git-code-michael <sudomichaelicloud@gmail.com>
2024-12-08 18:37:22 -0500

    Add functionality to get unique character counts

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     text_word_count = count_number_of_words(text)
+    unique_character_counts = get_unique_character_counts(text)
     
     print(text)
     print("")
     print(f"This text contains {text_word_count} words!")
+    print("")
+    print(f"The unique character counts of this text are:\n{unique_character_counts}")
 
 
 def get_book_text(path_to_file):
@@ -18,6 +21,18 @@ def count_number_of_words(string):
     words = string.split()
     word_count = len(words)
     return word_count
+
+
+def get_unique_character_counts(string):
+    character_count_dictionary = {}
+
+    for character in string.lower():
+        if character not in character_count_dictionary:
+            character_count_dictionary[character] = 1
+        else:
+            character_count_dictionary[character] += 1
+            
+    return character_count_dictionary
 
 
 main()
