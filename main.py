ea78869fe221c09d023fccac635fcbb373f30373
git-code-michael <sudomichaelicloud@gmail.com>
2024-12-08 17:47:55 -0500

    Add file reading functionality

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 def main():
-    pass
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    
+    print(text)
+    
+
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        file_contents = f.read()
+        return file_contents
 
 
 main()
